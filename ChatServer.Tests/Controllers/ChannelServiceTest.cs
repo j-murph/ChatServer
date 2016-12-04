6217f37cbefd4c9a6b8dad2c4ef479a8919012bc
j-murph <lone_geek@live.com>
2016-12-03 19:56:59 -0800

    Added unit tests for backend services.

---
+++ ChatServer.Tests/Controllers/ChannelServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatServer;
+using ChatServer.Controllers;
+using ChatServer.Services;
+
+namespace ChatServer.Tests.Controllers
+{
+    [TestClass]
+    public class ChannelServiceTest
+    {
+        [TestMethod]
+        public void SubscribeUser()
+        {
+            IChannelService cs = new ChannelService();
+            cs.SubscribeUser("testchannel", "testuser");
+        }
+
+        [TestMethod]
+        public void UnsubscribUser()
+        {
+            IChannelService cs = new ChannelService();
+            cs.UnsubscribeUser("testchannel", "testuser");
+        }
+
+        [TestMethod]
+        public void SubscribeUnsubscribeUser()
+        {
+            IChannelService cs = new ChannelService();
+            cs.SubscribeUser("testchannel", "testuser");
+            cs.UnsubscribeUser("testchannel", "testuser");
+        }
+    }
+}
---
+++ ChatServer.Tests/Controllers/MessengerServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatServer;
+using ChatServer.Services;
+
+namespace ChatServer.Tests.Controllers
+{
+    [TestClass]
+    public class MessengerServiceTest
+    {
+        [TestMethod]
+        public void BroadcastToChannel()
+        {
+            IMessengerService ms = new MessengerService();
+            ms.BroadcastToChannel("testchannel", "testuser", "test message.");
+        }
+
+        [TestMethod]
+        public void SendPrivateMessage()
+        {
+            IMessengerService ms = new MessengerService();
+            ms.SendPrivateMessage("testuser1", "testuser2", "test message.");
+        }
+
+        [TestMethod]
+        public void GetAllMessages()
+        {
+            IMessengerService ms = new MessengerService();
+            ms.GetAllMessages("testuser");
+        }
+    }
+}
