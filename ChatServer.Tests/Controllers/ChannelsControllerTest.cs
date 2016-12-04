6102eee6d90507cc3cff8e98582b0ac33d9050ed
j-murph <lone_geek@live.com>
2016-12-03 23:45:35 -0800

    Added ApiController unit tests

---
+++ ChatServer.Tests/Controllers/ChannelsControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatServer;
+using ChatServer.Services;
+using ChatServer.Controllers;
+
+namespace ChatServer.Tests.Controllers
+{
+    [TestClass]
+    public class ChannelsControllerTest
+    {
+        [TestMethod]
+        public void Subscribe()
+        {
+            IMessageService ms = new MessageService();
+            IChannelService cs = new ChannelService(ms);
+            var c = new ChannelsController(cs);
+            var result = c.Subscribe(new ChannelsController.SubscribeModel
+            {
+                Channel = "test channel",
+                User = "test user 123 $$$"
+            });
+
+            Assert.AreEqual(true, result.Success);
+        }
+
+        [TestMethod]
+        public void Unsubscribe()
+        {
+            IMessageService ms = new MessageService();
+            IChannelService cs = new ChannelService(ms);
+            var c = new ChannelsController(cs);
+
+            // Shouldn't error if the user is already unsubscribed.
+            var result = c.Unsubscribe(new ChannelsController.UnsubscribeModel
+            {
+                Channel = "test channel",
+                User = "test user 123 $$$"
+            });
+
+            Assert.AreEqual(true, result.Success);
+            
+            result = c.Subscribe(new ChannelsController.SubscribeModel
+            {
+                Channel = "test channel",
+                User = "test user 123 $$$"
+            });
+
+            Assert.AreEqual(true, result.Success);
+
+            // User is subscribed now.
+            result = c.Unsubscribe(new ChannelsController.UnsubscribeModel
+            {
+                Channel = "test channel",
+                User = "test user 123 $$$"
+            });
+
+            Assert.AreEqual(true, result.Success);
+        }
+
+        [TestMethod]
+        public void BroadcastMessage()
+        {
+            IMessageService ms = new MessageService();
+            IChannelService cs = new ChannelService(ms);
+            var c = new ChannelsController(cs);
+            var result = c.BroadcastMessage(new ChannelsController.BroadcastMessageModel
+            {
+                Channel = "test channel 123",
+                Message = "This is a message!",
+                User = "! @ # $ % ^ & * ( )"
+            });
+
+            Assert.AreEqual(true, result.Success);
+        }
+    }
+}
---
+++ ChatServer.Tests/Controllers/UsersControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatServer;
+using ChatServer.Services;
+using ChatServer.Controllers;
+
+namespace ChatServer.Tests.Controllers
+{
+    [TestClass]
+    public class UsersControllerTest
+    {
+        [TestMethod]
+        public void SendMessage()
+        {
+            IMessageService ms = new MessageService();
+            var c = new UsersController(ms);
+            var result = c.SendMessage(new UsersController.SendMessageModel
+            {
+                From = "user 11",
+                To = "user 2",
+                Message = "!! ## ';~"
+            });
+
+        }
+
+        [TestMethod]
+        public void GetAllMessages()
+        {
+            IMessageService ms = new MessageService();
+            var c = new UsersController(ms);
+            var result = c.GetMessages(new UsersController.GetMessagesModel
+            {
+                User = "test123"
+            });
+
+        }
+    }
+}
